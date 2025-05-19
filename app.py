139f285bba2f73df015078e11a18e328c9d55f04
unknown <prabu502@gmail.com>
2025-05-19 12:53:36 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+def add(a, b):
+   
+    return a + b
+
+def multiply(a, b):
+   
+    return a * b
+
+if __name__ == "__main__":
+    # Simple CLI interaction
+    print("Simple Calculator")
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    print(f"{x} + {y} = {add(x, y)}")
+    print(f"{x} * {y} = {multiply(x, y)}")
+
